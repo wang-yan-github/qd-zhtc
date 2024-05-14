c2602ec855f41a217b2bed43003682ddc16eb213
wang yan <wang.yan.workl@foxmail.com>
2024-05-14 11:02:35 +0800

    Tue May 14 11:02:35 CST 2024

--- vue-manage/vite.config.js
+++ vue-manage/vite.config.js
@@ -10,12 +10,12 @@ export default {
         host: '0.0.0.0',
         proxy: {
             '/dev': {
-                target: 'http://192.168.30.123:8082',
+                target: 'http://192.168.0.104:8081',
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/dev/, '')
             },
             '/api': {
-                target:'https://qd-zhtc',
+                target:'https://zhtc.aldwxa.top/api',
                 changeOrigin: true,
                 rewrite: path => path.replace(/^\/prod/, '')
             },
